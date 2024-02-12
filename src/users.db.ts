2d1675c728cd7fa4904e6e860d33da50e90b718f
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-02-13 02:30:33 +0300

    feat: implement users_DB

--- src/users.db.ts
+++ src/users.db.ts
@@ -1,5 +1,5 @@
-import { DataBase, User } from "./models/user.model";
-import { v4 as getUuid } from "uuid";
+import { DataBase, User } from './models/user.model';
+import { v4 as getUuid } from 'uuid';
 
 export const Users_DB = (() => {
   const dataBase: DataBase = [];
@@ -10,12 +10,25 @@ export const Users_DB = (() => {
     addUser: (user: User): User => {
       const id = getUuid();
       const { username, age, hobbies } = user;
-      const newUser: User = { id, username, age, hobbies }
+      const newUser: User = { id, username, age, hobbies };
       dataBase.push(newUser);
       return newUser;
     },
 
+    updateUser: (id: string, user: User): User => {
+      const { age, hobbies, username } = user;
+      const updatedUser: User = { id, username, age, hobbies };
+      const index = dataBase.findIndex((user) => user.id === id);
+      dataBase.splice(index, 1, updatedUser);
+      return updatedUser;
+    },
+
+    deleteUser: (id: string): void => {
+      const index = dataBase.findIndex((user) => user.id === id);
+      dataBase.splice(index, 1);
+    },
+
     getUserWithId: (id: string): User | null =>
       dataBase.find((user: User) => user.id === id) || null,
-  }
+  };
 })();
